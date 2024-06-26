b8abce26b7c1c3ca4b6d6bc5e5769ffc5b39f40d
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-06-26 01:19:00 -0400

    Build Creacion de dependencias
    - Se crearon y comprobaron la dependencias necesarias para el desarrollo del front-end. Vue y tailwind

---
+++ front/src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div class="flex bg-gray-700 p-10 justify-center">
+    <h1 class="text-fuchsia-600">Inicio del proyecto</h1>
+  </div>
+</template>
+
+
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+
+</style>
