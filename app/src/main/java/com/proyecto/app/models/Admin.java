9654dd309765fadf7089933bff04821212d1a5c1
Armin <102205057+Armanchas@users.noreply.github.com>
2024-06-28 18:12:57 -0400

    create admin model

---
+++ app/src/main/java/com/proyecto/app/models/Admin.java
@@ -0,0 +1,35 @@
+package com.proyecto.app.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 50, nullable = false)
+    private String username;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String lastname;
+
+    @Column(length = 50, nullable = false)
+    private String email;
+
+    @Column(length = 50, nullable = false)
+    private String password;
+
+}
