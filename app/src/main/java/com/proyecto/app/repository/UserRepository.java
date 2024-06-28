90793ee0c4678f7a81770a6d9756a39ae4e14c1d
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-06-27 20:31:11 -0400

    feat: Creacion<UserRepository>  y <UserService>
    -Creacion de UserRepository en repositories
    -Creacion de UserService en Services y sus funcionalidades CRUD

---
+++ app/src/main/java/com/proyecto/app/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.proyecto.app.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.proyecto.app.models.User;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
---
+++ app/src/main/java/com/proyecto/app/services/UserService.java
@@ -0,0 +1,51 @@
+package com.proyecto.app.services;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.proyecto.app.models.User;
+import com.proyecto.app.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    
+    @Autowired
+    private final UserRepository userRepository;
+
+    public List<User> getAll() throws Exception {
+        try {
+            return userRepository.findAll();
+        } catch (Exception e) {
+            throw new Exception("Error fetching data: "+e.getMessage());
+        }
+    }
+    
+    public User getUserByID(Long ID) throws Exception {
+        return userRepository.findById(ID).orElseThrow(
+            () -> new Exception("This user doesn't exist!") 
+        );
+    }
+    public void deleteUserByID(Long ID) throws Exception {
+        if (!userRepository.existsById(ID)) {
+            throw new Exception("This user doesn't exist!");
+        }
+        userRepository.deleteById(ID);
+    }
+    public User createUser(User userToCreate) throws Exception {
+        return userRepository.save(userToCreate);
+    }
+    public User updateUser(User userDetails) throws Exception {
+        User userToUpdate = userRepository.findById(userDetails.getId())
+        .orElseThrow(() -> new Exception("This user doesn't exist!"));
+
+        userToUpdate.setUsername(userToUpdate.getUsername());
+        userToUpdate.setName(userToUpdate.getName());
+        userToUpdate.setLastname(userToUpdate.getLastname());
+        userToUpdate.setEmail(userToUpdate.getEmail());
+        userToUpdate.setPassword(userToUpdate.getPassword());
+
+        return userRepository.save(userToUpdate);
+    }
+}
