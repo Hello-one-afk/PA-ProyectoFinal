4deb8b94ac38081ecc4a38bb83c5b066f37f3573
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-06-27 23:18:33 -0400

    Update Nota.java
    Creacion relacion con user

--- app/src/main/java/com/proyecto/app/models/Nota.java
+++ app/src/main/java/com/proyecto/app/models/Nota.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,9 +28,9 @@ public class Nota {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String contenido;
 
-    /*
+    
       @ManyToOne
       @JoinColumn(name = "usuario_id", nullable = false)
-      private Usuario usuario;
-     */
+      private User user;
+     
 }
