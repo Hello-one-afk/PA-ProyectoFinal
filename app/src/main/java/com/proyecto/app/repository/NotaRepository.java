39f6e975f4f0624f4de6bd51d5dcd054fd1e8b4c
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-07-03 16:21:17 -0400

    Update NotaRepository.java
    Se agrego metodo encontrar usuario por id , que tuviera asociada una nota en repository

--- app/src/main/java/com/proyecto/app/repository/NotaRepository.java
+++ app/src/main/java/com/proyecto/app/repository/NotaRepository.java
@@ -1,11 +1,15 @@
 package com.proyecto.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.proyecto.app.models.Nota;
 
 @Repository
-public interface NotaRepository extends JpaRepository<Nota, Integer>{
-    
+public interface NotaRepository extends JpaRepository<Nota, Integer> {
+
+        List<Nota> findByUserId(Long userId);
+
 }
